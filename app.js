408b84c36158a0fcac3a427190a1aa69ca87d19d
4thel00z <4thel00z@gmail.com>
2022-11-07 02:13:55 +0100

    [master] Add features to strip html via css selector
    Signed-off-by: 4thel00z <4thel00z@gmail.com>

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 #! /usr/bin/env node
 const { Command } = require('commander');
 const program = new Command();
-
+const fs  = require("node:fs");
+const { parse } = require('node-html-parser');
+const prerender = require('prerender');
 let captureWebsite = null;
+
 import('capture-website').then((importedModule) => {
     captureWebsite = importedModule;
-
     program
         .name('happyscreen')
         .description('check websites after update for changes')
@@ -20,6 +22,35 @@ import('capture-website').then((importedModule) => {
 
             captureWebsite.default.file(url, path) .then(console.log);
         });
+
+    program.command('strip-capture')
+        .description('strip certain selector and render')
+        .argument('<url>', 'path to html')
+        .argument('<selector>', 'css selector to strip')
+        .option('--out', 'save path in png')
+        .action((url, selector, options) => {
+            const path = options.out || "screenshot.png";
+            const html = fs.readFileSync(url,"utf-8");
+            const root = parse(html);
+            console.log(selector)
+            captureWebsite.default.file(root.removeChild(selector).toString(), path, {
+	inputType: 'html'
+} ).then(console.log);
+        });
+
+
+    program.command('start-prerender')
+        .description('start prerender server')
+        .option('--chrome', 'path to chrome browser')
+        .action((options) => {
+const server = prerender({
+
+ chromeLocation: options.chrome || '/usr/bin/chromium-browser'
+});
+server.start();    
+        });
+
+
 
     program.parse();
 });
